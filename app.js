7ab00cf8ddd1d49b06b45ef3d6e64271d6d46f31
Brontesewell <brontsewell7@gmail.com>
2020-05-07 16:06:39 -0700

    setting up epxress, nodejs and Graphql

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphql = require('express-graphql');
+const { buildSchema } = require('graphql')
+
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery{
+
+        }
+        type RootMutation{
+            
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {}
+}));
+
+app.listen(3000);
