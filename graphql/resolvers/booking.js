b6bf8b43657eb89bf47bd56173461780b5d6dc55
Brontesewell <brontsewell7@gmail.com>
2020-05-12 11:23:52 -0700

    authentication creater string changed and updated

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -22,7 +22,7 @@ module.exports = {
     }
     const fetchedEvent = await Event.findOne({ _id: args.eventId });
     const booking = new Booking({
-      user: '5c0fbd06c816781c518e4f3e',
+      user: req.userId,
       event: fetchedEvent
     });
     const result = await booking.save();
